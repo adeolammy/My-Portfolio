587db189153907cbc4883e322b5cd688bdb11968
adeolammy <adeolammy2020@gmail.com>
2023-06-05 12:27:27 +0100

    Adding the footer section to the project

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {IoLogoFacebook} from 'react-icons/io'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <footer>
+      <a href="#" className="footer_logo">ADEBAJO ADEWALE</a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimainials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer_socials">
+        <a href=" https://facebook.com"><IoLogoFacebook/></a>
+        <a href=" https://instagram.com"><FiInstagram/></a>
+        <a href=" https://twitter.com"><IoLogoTwitter/></a>
+      </div>
+      <div className="footer_copyright">
+        <small>&copy; Adebajo All rights reserved</small>
+      </div>
+    </footer>
   )
 }
 
